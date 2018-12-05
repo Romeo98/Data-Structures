3c0b12cf2c0bef7ad806f6972f0710190a425acd
Romeo98 <39018349+Romeo98@users.noreply.github.com>
2018-12-04 22:25:18 -0600

    Adding single-linked simple list
    Only has the most basic options

---
+++ single-simple.cpp
@@ -0,0 +1,236 @@
+// Single-linked simple list functions
+
+#include <iostream>
+
+struct Node {
+	int data;
+	Node *next;
+};
+
+class List {
+
+	private:
+		Node *Head;
+	
+	public:
+		List();
+
+		void Node_insFirst(int &);
+		void Node_insLast(int &);
+		void Node_insBefore(int &, int &);
+		void Node_insAfter(int &, int &);
+		void Node_Delete(int &);
+
+		void List_Delete();
+		void List_Show();
+		void List_recShow();
+
+		void recDelete(Node *);
+		void recShow(Node *);
+};
+
+
+
+int main() {
+	List Object;
+	int data, dataRF;
+	char option;
+
+	do {
+		std::cout << "MENU\n\n";
+		std::cout << "1) Insert node at the beginning\n";
+		std::cout << "2) Insert node at the end\n";
+		std::cout << "3) Insert node before\n";
+		std::cout << "4) Insert node after\n";
+		std::cout << "5) Delete node\n";
+		std::cout << "6) Delete list\n";
+		std::cout << "7) Show list\n";
+		std::cout << "8) Show inverted list\n";
+		std::cout << "9) Salir\n\n";
+		std::cout << "Your option is: ";
+
+		std::cin >> option;
+		system("clear");
+
+		if(option != '9') 
+
+			switch(option) {
+				
+				case '1': 
+					std::cout << "Exit ctrl-d\n";
+					while(std::cin >> data)
+						Object.Node_insFirst(data);
+					break;
+				
+				case '2':
+					std::cout << "Exit ctrl-d\n";
+					while(std::cin >> data)
+						Object.Node_insLast(data);
+					break;
+				
+				case '3':
+					std::cout << "Enter data & datarRF\n";
+					std::cin >> data >> dataRF;
+					Object.Node_insBefore(data, dataRF);
+					break;
+				
+				case '4':
+					std::cout << "Enter data & datarRF\n";
+					std::cin >> data >> dataRF;
+					Object.Node_insAfter(data, dataRF);
+					break;
+				
+				case '5':
+					std::cout << "Enter dataRF\n";
+					std::cin >> dataRF;
+					Object.Node_Delete(dataRF);
+					break;
+				
+				case '6':
+					Object.List_Delete();
+					break;
+				
+				case '7':
+					Object.List_Show();
+					break;
+				
+				case '8':
+					Object.List_recShow();
+					break;
+			}
+
+		else 
+			break;
+		
+		std::cout << '\n';
+		Object.List_Show();
+		std::cin.clear();
+		std::cin.ignore();
+
+	} while(true);
+}
+
+
+
+List::List() {
+	Head = NULL;
+}
+
+void List::Node_insFirst(int &data) {
+	Node *New = new Node;
+	New->data = data;
+	New->next = Head;
+	Head = New;
+}
+
+void List::Node_insLast(int &data) {
+	if(!Head) 
+		return List::Node_insFirst(data);
+	
+	Node *Temp = Head;
+
+	while(Temp->next)
+		Temp = Temp->next;
+	
+	Node *New = new Node;
+	New->data = data;
+	New->next = Temp->next;
+	Temp->next = New;
+}
+
+void List::Node_insBefore(int &data, int &dataRF) {
+	if(!Head) 
+		return List::Node_insFirst(data);
+	
+	Node *Temp = Head;
+
+	if(Temp->data == dataRF)
+		return List::Node_insFirst(data);
+
+	while(Temp->next && (Temp->next)->data != dataRF)
+		Temp = Temp->next;
+	
+	if( (Temp->next)->data == dataRF) {
+		Node *New = new Node;
+		New->data = data;
+		New->next = Temp->next;
+		Temp->next = New;
+	}
+}
+
+void List::Node_insAfter(int &data, int &dataRF) {
+	if(!Head) 
+		return List::Node_insFirst(data);
+	
+	Node *Temp = Head;
+
+	while(Temp->next && Temp->data != dataRF)
+		Temp = Temp->next;
+	
+	if(Temp->data == dataRF) {
+		Node *New = new Node;
+		New->data = data;
+		New->next = Temp->next;
+		Temp->next = New;
+	}
+}
+
+void List::Node_Delete(int &dataRF) {
+	if(!Head)
+		return;
+	
+	Node *Temp1 = Head, *Temp2 = NULL;
+
+	if(Temp1->data == dataRF) {	
+		Head = Head->next;
+		delete(Temp1);
+		return;
+	}
+
+	while(Temp1->next && (Temp1->next)->data != dataRF) 
+		Temp1 = Temp1->next;
+	
+	if(Temp1->next) {
+		Temp2 = Temp1->next;
+		Temp1->next = (Temp1->next)->next;
+		delete(Temp2);
+	}
+}
+
+void List::List_Delete() {
+	List::recDelete(Head);
+	Head = NULL;
+}
+
+void List::recDelete(Node *Temp) {
+	if(Temp) {
+		List::recDelete(Temp->next);
+		delete(Temp);
+	}
+}
+
+void List::List_Show() {
+
+	if(!Head) {
+		std::cout << "Empty list\n\n";
+		return;
+	}
+
+	Node *Temp = Head;
+
+	while(Temp) {
+		std::cout << Temp->data << '\n';
+		Temp = Temp->next;
+	}
+}
+
+void List::List_recShow() {
+	List::recShow(Head);
+}
+
+void List::recShow(Node *Temp) {
+	if(Temp) {
+		List::recShow(Temp->next);
+		std::cout << Temp->data << '\n';
+	}
+}
